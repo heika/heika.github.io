9ae17835098cd24375930b5f5040dcac6b536a7c
heika <lkhahaha@gmail.com>
2019-04-26 18:47:16 +0800

    remove useless files

--- newstyle/js/common.js
+++
@@ -1,7 +0,0 @@
-var userLang = (navigator.language) ? navigator.language : navigator.userLanguage;
-
-$(document).ready(function() {
-    $("body").on("contextmenu", "img", function(e) {
-        return false;
-      });
-});
